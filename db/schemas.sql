6152b852b12a6182c2dec139fe3c55a6cc300699
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-02-02 19:45:25 -0800

    Added DROPS for tables and database, and Added employees table

--- db/schemas.sql
+++ db/schemas.sql
@@ -1,16 +1,30 @@
+DROP DATABASE IF EXISTS empoylee_db;
 CREATE DATABASE empoylee_db;
 
 USE empoylee_db;
 
+DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(30)
 );
 
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+DROP TABLE IF EXISTS role;
 CREATE TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255),
-  salary INT,
+  title VARCHAR(30),
+  salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
